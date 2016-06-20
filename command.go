8b5e2150ece09e34a828115ef37ae5e81310c64b
wanlipeng <maisonwan@gmail.com>
2016-06-20 16:40:34 +0800

    规范代码格式

--- command.go
+++ command.go
@@ -3,15 +3,15 @@ package main
 // 命令行解析内容
 type Command struct {
 	// 是否是帮助的标识
-	helpFlag bool
+	helpFlag    bool
 	// 版本标识
 	versionFlag bool
 	// 类路径
-	cpOption string
+	cpOption    string
 	// jre路径
-	XjreOption string
+	XjreOption  string
 	// 运行的主类class
-	class string
+	class       string
 	// Java运行参数
-	args []string
+	args        []string
 }
