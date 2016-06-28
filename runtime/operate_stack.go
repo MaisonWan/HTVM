43f76f0548a5cbb0524bea4dbd810ed53a60c452
wanlipeng <maisonwan@gmail.com>
2016-06-28 21:05:25 +0800

    增加操作数的栈，以及里面的接口

---
+++ runtime/operate_stack.go
@@ -0,0 +1,78 @@
+package runtime
+
+import "math"
+
+// 操作数栈
+type OperateStack struct {
+	size  uint
+	slots []Slot
+}
+
+// 创建一个新的操作数栈
+func newOperateStack(maxStack uint) *OperateStack {
+	if maxStack > 0 {
+		return &OperateStack{
+			slots:make([]Slot, maxStack),
+		}
+	}
+	return nil
+}
+
+func (self *OperateStack) PushInt(value int32) {
+	self.slots[self.size].number = value
+	self.size++
+}
+
+func (self *OperateStack) PopInt() int32 {
+	self.size--
+	return self.slots[self.size].number
+}
+
+func (self *OperateStack) PushFloat(value float32) {
+	bits := math.Float32bits(value)
+	self.slots[self.size].number = int32(bits)
+	self.size++
+}
+
+func (self *OperateStack) PopFloat() float32 {
+	self.size--
+	n := self.slots[self.size].number
+	return math.Float32frombits(n)
+}
+
+func (self *OperateStack) PushLong(value int64) {
+	self.slots[self.size].number = int32(value)
+	self.slots[self.size + 1].number = int32(value >> 32)
+	self.size += 2
+}
+
+func (self *OperateStack) PopLong() int64 {
+	self.size -= 2
+	low := uint32(self.slots[self.size].number)
+	high := uint32(self.slots[self.size + 1].number)
+	return (int64(high) << 32) | int64(low)
+}
+
+func (self *OperateStack) PushDouble(value float64) {
+	bits := math.Float64bits(value)
+	self.PushLong(int64(bits))
+}
+
+func (self *OperateStack) PopDouble() int64 {
+	bits := uint64(self.PopLong())
+	return math.Float64frombits(bits)
+}
+
+// 引用类型
+func (self *OperateStack) PushRef(ref *Object) {
+	self.slots[self.size].ref = ref
+	self.size++
+}
+
+// 弹出引用，记得置位为nil，内存释放
+func (self *OperateStack) PopRef() *Object {
+	self.size--
+	ref := self.slots[self.size].ref
+	self.slots[self.size].ref = nil
+	return ref
+}
---
+++ runtime/slot.go
@@ -0,0 +1,66 @@
+package runtime
+
+import (
+	"math"
+)
+
+// 同时容纳int和引用
+type Slot struct {
+	number int32
+	ref    *Object
+}
+
+// 局部变量表
+type LocalVars []Slot
+
+func newLocalVars(maxLocals uint) LocalVars {
+	if maxLocals > 0 {
+		return make([]Slot, maxLocals)
+	}
+	return nil
+}
+
+// 设置整形
+func (self LocalVars) SetInt(index uint, value int32) {
+	self[index].number = value
+}
+
+func (self LocalVars) GetInt(index uint) {
+	return self[index].number
+}
+
+func (self LocalVars) SetFloat(index uint, value float32) {
+	bits := math.Float32bits(value)
+	self[index].number = uint32(bits)
+}
+
+func (self LocalVars) GetFloat(index uint) float32 {
+	bits := int32(self[index].number)
+	return math.Float32bits(bits)
+}
+
+// 设置长整形，需要两个int的空间
+func (self LocalVars) SetLong(index uint, value int64) {
+	self[index].number = int32(value)
+	self[index + 1].number = int32(value >> 32)
+}
+
+// 得到长整型
+func (self LocalVars) GetLong(index uint) int64 {
+	low := self[index].number
+	high := self[index + 1].number
+	return (high << 32) | low
+}
+
+// 写入64位浮点
+func (self LocalVars) SetDouble(index uint, value float64) {
+	bits := math.Float64bits(value)
+	self.SetLong(index, uint64(bits))
+}
+
+// 得到浮点数
+func (self LocalVars) GetDouble(index uint) float64 {
+	bits := uint64(self.GetLong(index))
+	return math.Float64frombits(bits)
+}
+
